86c0b3c3cd9e7618c9f69e61ca758f44bd81db98
taverna.ricardo <taverna.ricardo@grupomarista.org.br>
2020-01-02 09:55:26 -0300

    Create Repeting StreamField Content

---
+++ streams/blocks.py
@@ -0,0 +1,42 @@
+"""Streamfields live in here"""
+
+from wagtail.core import blocks
+
+
+class TitleAndTextBlock(blocks.StructBlock):
+    """ Title and text and noting else """
+
+    title = blocks.CharBlock(required=True, help_text='Add your title')
+    text = blocks.TextBlock(required=True, help_text='Add aditional text')
+
+    class Meta: 
+        template = "streams/title_and_block.html"
+        icon = "edit"
+        label = "Title & Text"
+
+
+class RichtextBlock(blocks.RichTextBlock):
+    """ Richtext with all the features"""
+    
+
+    class Meta: 
+        template = "streams/richtext_block.html"
+        icon = "doc-full"
+        label = "Full Richtext"
+
+
+class SimpleRichtextBlock(blocks.RichTextBlock):
+    """ Richtext without all the features"""
+
+    def __init__(self, required=True, help_text=None, editor='default', features=None, validators=(), **kwargs):
+        super().__init__(**kwargs)
+        self.features = [
+            "bold",
+            "italic",
+            "link",
+        ]        
+
+    class Meta: 
+        template = "streams/richtext_block.html"
+        icon = "edit"
+        label = "Simple Richtext"
