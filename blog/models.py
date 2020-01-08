4237c4534652c7f24fcff99b0aadadf185775f41
taverna.ricardo <taverna.ricardo@grupomarista.org.br>
2020-01-08 11:03:34 -0300

    USING A SNIPPETCHOOSERPANEL

--- blog/models.py
+++ blog/models.py
@@ -1,15 +1,30 @@
 from django.db import models
 from django.shortcuts import render
 
-from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel, MultiFieldPanel
+from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel, MultiFieldPanel, InlinePanel
 from wagtail.contrib.routable_page.models import RoutablePageMixin, route
-from wagtail.core.models import Page
+from wagtail.core.models import Page, Orderable
 from wagtail.core.fields import StreamField
 from wagtail.images.edit_handlers import ImageChooserPanel
 from wagtail.snippets.models import register_snippet
+from wagtail.snippets.edit_handlers import SnippetChooserPanel
 
+from modelcluster.fields import ParentalKey
 
 from streams import blocks
+
+
+class BlogAuthorOrderable(Orderable):
+    page = ParentalKey("blog.BlogDetailPage", related_name="blog_authors")
+    author = models.ForeignKey(
+        "blog.BlogAuthor",
+        on_delete=models.CASCADE,
+    )
+
+    panels = [
+        SnippetChooserPanel("author"),
+
+    ]
 
 
 class BlogAuthor(models.Model):
@@ -120,5 +135,11 @@ class BlogDetailPage(Page):
     content_panels = Page.content_panels + [
         FieldPanel("custom_title"),
         ImageChooserPanel("blog_image"),
+        MultiFieldPanel(
+            [
+                InlinePanel("blog_authors", label="Author", min_num=1, max_num=4)
+            ],
+            heading="Author(s)"
+        ),
         StreamFieldPanel("content"),
     ]
