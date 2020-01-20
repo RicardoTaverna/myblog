d73ede8ac49a6d2adb8b69610ac63adb39253fe7
taverna.ricardo <taverna.ricardo@grupomarista.org.br>
2020-01-20 13:48:04 -0300

    Using Wagtails save() method to remove template caching

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,7 @@
 from django import forms
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.core.cache import cache
+from django.core.cache.utils import make_template_fragment_key
 from django.db import models
 from django.shortcuts import render
 
@@ -196,6 +198,11 @@ class BlogDetailPage(Page):
         ),
         StreamFieldPanel("content"),
     ]
+
+    def save(self, *args, **kwargs):       
+        key = make_template_fragment_key("blog_post_preview", [self.id])
+        cache.delete(key) 
+        return super().save(*args, **kwargs)
 
 
 # First subclasse blog page
